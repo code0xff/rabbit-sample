2da7086a2dd6801f092ccb528f0a38314d1362a6
jungyongum <ian.jungyong.um@gmail.com>
2021-07-26 14:32:00 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const amqp = require('amqplib/callback_api');
+const url = 'amqp://rabbitmq:rabbitmq@localhost:5672';
+const queue = 'ufc';
+
+amqp.connect(url, function (error0, connect) {
+    if (error0) {
+        console.log(error0);
+        return;
+    }
+    connect.createChannel(function (error1, channel) {
+        if (error1) {
+            console.log(error1);
+            return;
+        }
+
+        channel.assertQueue(queue, {durable: true});
+
+        console.log("[*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function (msg) {
+            const json = JSON.parse(msg.content.toString());
+            console.log(json);
+        }, {
+            noAck: true
+        });
+    });
+});
